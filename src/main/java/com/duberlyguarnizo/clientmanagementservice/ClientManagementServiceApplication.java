3ff1ba3f10f8267da064e3346148a894178d5a21
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2023-09-19 20:07:18 -0500

    Initial commit

---
+++ src/main/java/com/duberlyguarnizo/clientmanagementservice/ClientManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.clientmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientManagementServiceApplication.class, args);
+    }
+
+}
